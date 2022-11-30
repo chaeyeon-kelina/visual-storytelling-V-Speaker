9432bc4c1fa3aa777af4afb2b73d0ab9ca9ebdac
neoseurae12 <87654809+neoseurae12@users.noreply.github.com>
2022-11-30 13:58:08 +0900

    +turn page

--- flipbook_ver2.js
+++ flipbook_ver2.js
@@ -3,28 +3,34 @@ var flipbookEL = document.getElementById("flipbook");
 window.addEventListener("resize", function (e) {
   flipbookEL.style.width = "";
   flipbookEL.style.height = "";
-  $(flipbookEL).turn("size", flipbookEL.clientWidth, flipbookEL.clientHeight);
+  $(flipbookEL).turn(
+    "size",
+    // flipbookEL.clientWidth * 1.8,
+    "auto",
+    flipbookEL.clientHeight * 0.8
+    // "108px"
+  );
 });
 
 $(flipbookEL).turn({
   autoCenter: true,
-  width: 175 * 6,
-  height: 91 * 6,
+  // width: 100%,
+  // height: 100%,
 });
 
-$(function () {
-  //Keep track of last scroll
-  var lastScroll = 0;
-  $(window).scroll(function (event) {
-    //Sets the current scroll position
-    var st = $(this).scrollTop();
-    //Determines up-or-down scrolling
-    if (st > lastScroll) {
-      //downward-scrolling
-    } else {
-      //upward-scrolling
-    }
-    //Updates scroll position
-    lastScroll = st;
-  });
-});
+function addPage(page, book) {
+  // 	First check if the page is already in the book
+  if (!book.turn("hasPage", page)) {
+    // Create an element for this page
+    var element = $("<div />", {
+      class: "page " + (page % 2 == 0 ? "odd" : "even"),
+      id: "page-" + page,
+    }).html('<i class="loader"></i>');
+    // If not then add the page
+    book.turn("addPage", element, page);
+    // Let's assum that the data is comming from the server and the request takes 1s.
+    setTimeout(function () {
+      element.html('<div class="data">Data for page ' + page + "</div>");
+    }, 1000);
+  }
+}
