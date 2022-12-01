628e55dd4b0ee0c9867842a2819177a50b87714b
Yewon Lee <111197714+eyewon@users.noreply.github.com>
2022-12-02 02:44:04 +0900

    스크롤 오류 수정 + 멤버스 제목 추가

--- main.js
+++ main.js
@@ -14,12 +14,18 @@ mHtml.animate({ scrollTop: 0 }, 10);
 $(window).on("wheel", function (e) {
   if (mHtml.is(":animated")) return;
   if (e.originalEvent.deltaY > 0) {
-    if (page == 18) return;
+    if (page == 3) return;
     page++;
   } else if (e.originalEvent.deltaY < 0) {
     if (page == 1) return;
     page--;
   }
-  var posTop = (page - 1) * $(window).height();
+  if (page == 1) {
+    var posTop = (page - 1) * ($(window).height() + 50);
+  } else if (page == 2) {
+    var posTop = (page - 1) * ($(window).height() + 50);
+  } else {
+    var posTop = (page - 1) * ($(window).height() + 170);
+  }
   mHtml.animate({ scrollTop: posTop });
 });
